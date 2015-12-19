2c5971173f48816a19f6d3186a5e915b9ff007cb
Rohit <rohit11may@gmail.com>
2015-12-19 17:11:40 +0000

    density calculation

--- Shower Density.py
+++ Shower Density.py
@@ -11,10 +11,22 @@ fileName = "\\0.1.1_07_2011-22_01_2012_allCSV.txt"
 tempFilePath = (os.path.abspath(os.path.join(inspect.getfile(inspect.currentframe()), os.pardir))).split("\\")
 fileVar = open("\\".join(tempFilePath[0:len(tempFilePath)-1]) + fileName)
 
-print(fileVar)
-fileList = []
+density = []
+dateStr = fileVar.readline().split(',')[0:2]
+print(dateStr)
+dateStr = dateStr[0] + ',' + dateStr[1][0:2]
+currentdate_object = datetime.strptime(dateStr, '%Y/%m/%d,%H')
+count = 1
 for x in fileVar:
 	y  = x.split(',')
-	dateStr = y[0] + ',' + y[1]
-	date_object = datetime.strptime(dateStr, '%Y/%m/%d,%H:%M:%S')
-	
+	dateStr = y[0] + ',' + y[1][0:2]
+	newdate_object = datetime.strptime(dateStr, '%Y/%m/%d,%H')
+	if newdate_object == currentdate_object:
+		count += 1
+	else:
+		density.append({currentdate_object: count})
+		currentdate_object = newdate_object
+		count = 1
+
+for z in density:
+	print(z)
